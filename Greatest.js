5292977620f4cfbdb71b96ce17deb8a4024d83dd
Prashant <prashantd049@gmail.com>
2023-01-03 19:48:56 +0530

    Greatest

---
+++ Greatest.js
@@ -0,0 +1,15 @@
+import java.io.*;
+
+    public static void main(String[] args)
+
+	{
+		int a, b, c;
+
+		int largest;
+		a = 5;
+		b = 10;
+		c = 3;
+		largest = biggestOfThree(a, b, c);
+		System.out.println(largest + " is the largest number.");
+	}
+}
