d2c182229d4866876c4e7ed3d069fa1558bdd5c5
Prashant <prashantd049@gmail.com>
2023-01-04 21:59:30 +0530

    Pattern

---
+++ pattern.js
@@ -0,0 +1,21 @@
+import java.io.*;
+ 
+public class GeeksForGeeks {
+    public static void StarRightTriangle(int n)
+    {
+        int a, b;
+        for (a = 0; a < n; a++) {
+            for (b = 0; b <= a; b++) {
+                System.out.print("* ");
+            }
+ 
+            System.out.println();
+        }
+    }
+ 
+    public static void main(String args[])
+    {
+        int k = 5;
+        StarRightTriangle(k);
+    }
+}
